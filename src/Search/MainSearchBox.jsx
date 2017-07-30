9245d949d315ffbb3348955f52033f91e180de5e
Jeff An <uwjeffan@outlook.com>
2017-07-30 02:05:04 -0400

    Added visibility toggle for the examples

--- src/Search/MainSearchBox.jsx
+++ src/Search/MainSearchBox.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
+import $ from 'jquery';
 import './MainSearchBox.css';
 import MainSearchBar from './SearchBar.jsx';
 
 let bullet = String.fromCharCode(9679);
 
 class MainSearchBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-    }
     render() {
         return (
             <div className = 'main-search-box well well-lg'>
@@ -23,23 +18,47 @@ class MainSearchBox extends React.Component {
 }
 
 class SearchIntro extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayExamples: "none"
+        };
+        this.toggleExamples = this.toggleExamples.bind(this);
+    }
+
+    toggleExamples() {
+        if (this.state.displayExamples === 'none') {
+            $('.main-search-box').css('min-height', '385px');
+            $('.main-search-box').css('height', '50%');
+            this.setState(prev => ({
+                displayExamples: "block"
+            }));
+        } else {
+            $('.main-search-box').css('min-height', '300px');
+            $('.main-search-box').css('height', '45%');
+            this.setState(prev => ({
+                displayExamples: "none"
+            }));
+        }
+    }
+
     render() {
         return (
             <div className = {this.props.group + '-intro'}>
-                <h3>Simple, Smart</h3>
-                <h1 style={{ margin: '0'}}>
+                <h3 style = {{ marginTop: '25px'}}>Simple, Smart</h3>
+                <h1 style = {{ margin: '0 0 5 0'}}>
                     Concept Mapping.
                 </h1>
                 <br/>
-                <br/>
-                <div className="regular-font" >
-                    IdeaMap generates concept maps showing related and relevant topics according to statistical analysis.
+                <div className = "bold-font">
+                    <a>Instructions</a>
                 </div>
-                <br/>
-                <div className="bold-font">
-                    Examples:
+                <div className="regular-font-lg" style = {{ margin: '0 0 5 0'}} >
+                    IdeaMap finds ideas related to your topic and uses them to construct concept maps. Click
+                    {' '} <a onClick = { this.toggleExamples }>here</a> {' '}
+                    to view examples.
                 </div>
-                <div className="regular-font">
+                <div id='search-examples' style={{ display:this.state.displayExamples }} className="regular-font-lg">
                     <div>{bullet}  <a>Trigonometry</a></div>
                     <div>{bullet}  <a>World War II</a></div>
                     <div>{bullet}  <a>Game of Thrones</a></div>
@@ -47,6 +66,7 @@ class SearchIntro extends React.Component {
             </div>
         );
     }
+
 }
 
 export { MainSearchBox as default };
